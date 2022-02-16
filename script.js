e2edcb70453ce81c12162e206160c54ca54c030a
Brittany Craig <brittanycraig@gmail.com>
2022-02-16 13:32:17 -0500

    added somes styling

--- script.js
+++ script.js
@@ -5,12 +5,13 @@
 const url = "http://localhost:3000/notes";
 const form = document.getElementById('note-form')
 const noteList = document.getElementById('notes-list')
+//const created_at: (""
 
 //listen for form submit
 form.addEventListener('submit', function(e) {
     e.preventDefault()
     //grab the value from the input
-    const noteTitle = document.getElementById('note-title').value 
+    const noteTitle = document.querySelector('#note-title').value
     const noteText = document.querySelector('#note-text').value
     fetch(url, {
         method: 'POST',
@@ -19,7 +20,7 @@ form.addEventListener('submit', function(e) {
             //I don't have to include "id" here bc json server adds it for me
             title: noteTitle,
             body: noteText,
-            created_at: moment().format(),
+            //created_at: moment().format(),
         }),
     })
         .then((res) => res.json())
@@ -43,18 +44,20 @@ function listNotes () { //see line 41
         })  
 }
 
-listNotes()
-
 //code block below from Paul- need to remove and try this myself tomorrow
 function renderListItem(listObj) {
-    const noteEl = document.createElement("li");
+    const noteEl = document.createElement("li")
     noteEl.id = listObj.id;
-    noteEl.classList.add("noted");
-    noteEl.innerHTML = `<input type="radio" name="note" id="${listObj.id}" class="radio"><h3 class="title" id="${listObj.id}">${listObj.title}</h3><span class= "bodyNote" id="${listObj.id}">${listObj.body}</span><span id="${listObj.id}" class="date">${listObj.date}</span>`;
-    noteList.appendChild(noteEl);
+    // noteEl.classList.add("noted");
+    noteEl.innerHTML = `
+        
+        <h3 class="title" id="return-title" id="${listObj.id}">${listObj.title}</h3>
+        <span class= "bodyNote" id="${listObj.id}">${listObj.body}</span>
+        `
+    noteList.appendChild(noteEl)
 }
-
-
+//<input name="note" id="${listObj.id}" class="radio"></input>
+//<span id="${listObj.id}" class="created_at">${listObj.date}</span>//
 /***** DOM changing functions *****/
 
 // Add one todo item to the list on the page
@@ -66,6 +69,6 @@ form.reset()
   /**** Function that runs as soon as the script file loads *****/
   // call this when the script first runs (on page load)
   // This runs only on the first load!
-//listNotes()
+listNotes()
 
 // PATCH request Localhost:3000/notes/1 <==make sure to hvae the iD!!!!
