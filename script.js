d4c935f6d997544ca8761a6f9e7708b585548673
Brittany Craig <brittanycraig@gmail.com>
2022-02-17 10:33:04 -0500

    delete button + function working

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 const url = "http://localhost:3000/notes";
 const form = document.getElementById('note-form') //line 20-27 in html
 const noteList = document.getElementById('notes-list-ul') //this is the notes container
-//const eachList = document.getElementsByClassName('li')
+//let eachList = document.getElementsByClassName('li').
 
 //listen for form submit
 form.addEventListener('submit', function(e) {
@@ -44,17 +44,22 @@ function listNotes () { //see line 41
         })  
 }
 
-
 // Add one todo item to the list on the page
 //create the title, text, and date for each note
 function renderListItem(listObj) {
     const noteEl = document.createElement("li")
     noteEl.id = listObj.id;
+    // noteEl.innerHTML = `
+    //     <h3 class="title" id="return-title" id="${listObj.id}">${listObj.title}</h3>
+    //     <h5 class="created_at" id="${listObj.id}">Posted: ${listObj.created_at}</h5>
+    //     <h4 class= "bodyNote" id="${listObj.id}">${listObj.body}</h4> </br>
+    //     <h6 class= "icons"  id="${listObj.id}"><i class="fa-regular fa-pen-to-square"></i> <i class="fa-solid fa-trash"></i></h6>
+    //     `
     noteEl.innerHTML = `
-        <h3 class="title" id="return-title" id="${listObj.id}">${listObj.title}</h3>
-        <h5 class="created_at" id="${listObj.id}">Posted: ${listObj.created_at}</h5>
-        <h4 class= "bodyNote" id="${listObj.id}">${listObj.body}</h4> </br>
-        <h6 class= "icons" id="icons" id="${listObj.id}"><i class="fa-regular fa-pen-to-square"></i> <i class="fa-solid fa-trash"></i></h6>
+        <h2 class="title" id="return-title">${listObj.title}</h2>
+        <h3 class="created_at">Posted: ${listObj.created_at}</h3>
+        <h4 class= "bodyNote">${listObj.body}</h4> </br>
+        <p><i class="fa-regular fa-pen-to-square edit"></i> <i class="fa-solid fa-trash delete"></i></p>
         `
     noteList.prepend(noteEl) // this puts the new note at the beginning instead of the end
 }
@@ -67,28 +72,31 @@ function clearInputs() {
   // call this when the script first runs (on page load)
   // This runs only on the first load!
 listNotes()
+//id="icons"
 
-//Delete a note
-
+//Event listener for click on edit or trash
 noteList.addEventListener('click', function(e) {
     e.preventDefault()
     console.log('you clicked')
-    if (e.target.tagName === "I") {
-        console.log ('you found the trash icon') //currently finding both icons
-        deleteList(e.target)
+    if (e.target.classList.contains('delete')) {
+        console.log ('you found the trash icon') 
+        deleteNote(e.target)
+    } if (e.target.classList.contains('edit')) {
+        console.log ('you found the edit icon') 
+        alert("Oh you wanted to edit, TOO BAD!!!")
     }
 })
         
-function deleteList(element) {
+function deleteNote(element) {
     //need to know which note to delete, so need the id (matching in db.json)
     const listId = element.parentElement.id //this pulls 'icons'...is that right?
-    fetch(`http://localhost:3000/notes/${listId}`, { //this isn't right
+    fetch(`http://localhost:3000/notes/${listId}`, { //this pulls each individual id that matches in the db.json
+        method: 'DELETE',    
     }).then(function () {
-       noteList.parentNode.removeChild(noteList)//deletes all the notes 
-        // element.parentElement.remove()//deletes the icons only
+        element.parentElement.remove() //removes the clicked on note
     })
 }
-
+//Next: Work on the edit features
 
 
 // PATCH request Localhost:3000/notes/1 <==make sure to hvae the iD!!!!
